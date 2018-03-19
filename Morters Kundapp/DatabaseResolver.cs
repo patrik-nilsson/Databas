e5f4adcdf10db2e1428eda4595957b2c37e413bf
Patrik Nilsson <patriknilson@hotmail.com>
2018-03-19 14:47:34 +0100

    Now using Monogame
    !

---
+++ Morters Kundapp/DatabaseResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Threading;
+
+namespace Morters_Kundapp
+{
+    static class DatabaseResolver
+    {
+        static bool boolfound = false;
+        static NpgsqlConnection conn = new NpgsqlConnection("Server=pgserver.mah.se; Port=5432; UserId = ah7326; Password = emmodj9b; Database = ah7326");
+    
+        public static void Connect()
+        { 
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT version(); ", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                boolfound = true;
+                Console.WriteLine("connectionestablished");
+                Console.WriteLine("{0}", dr[0]);
+            }
+            if (boolfound == false)
+            {
+                Console.WriteLine("Data does not exist");
+            }
+            dr.Close();
+            using (var cmd2 = new NpgsqlCommand("SELECT * FROM Tavling", conn))
+            {
+                using (var reader = cmd2.ExecuteReader())
+                {
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(1));
+                }
+            }
+            dr.Close();
+        }
+    }
+}
---
+++ Morters Kundapp/Game1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Morters_Kundapp
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                DatabaseResolver.Connect();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightGray);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
